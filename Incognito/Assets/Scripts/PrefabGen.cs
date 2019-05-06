81ca1ab63162068c1c2826192e24c288b7f8c403
ArtixBot <williamwongcastle@gmail.com>
2019-05-06 15:09:35 -0400

    Layer handling

--- Incognito/Assets/Scripts/PrefabGen.cs
+++ Incognito/Assets/Scripts/PrefabGen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 
@@ -12,28 +13,49 @@ public class PrefabGen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // TXT Parsing
-        // string path = "Assets/Prefabs/prefab_test.txt";
-        // StreamReader reader = new StreamReader(path); 
-        // string ASCII_codes = reader.ReadToEnd();
-        // byte[] asciiBytes = Encoding.UTF8.GetBytes(ASCII_codes);
-        // for(int i = 0; i < asciiBytes.Length; i++){
-        //     Debug.Log("Tile " + i + ": " + asciiBytes[i]);
-        // }
-        // reader.Close();
         
-        // Rudimentary .xp parser.
+        // .xp file format, as per manual specifications:
+        // #-----xp format version (32)
+        // A-----number of layers (32)
+        //  /----image width (32)
+        //  |    image height (32)
+        //  |  /-ASCII code (32) (little-endian!)
+        // B|  | foreground color red (8)
+        //  |  | foreground color green (8)
+        //  |  | foreground color blue (8)
+        //  | C| background color red (8)
+        //  |  | background color green (8)
+        //  \--\-background color blue (8)
+        
+        // Rudimentary .xp parser. Handles single-layer images.
         // It actually works, somehow...
-        FileInfo path = new FileInfo("Assets/Prefabs/prefab_test.xp");
+        FileInfo path = new FileInfo("Assets/Prefabs/prefab_test_3.xp");
         FileStream originalFileStream = path.OpenRead();
         GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress);
 
         MemoryStream bigStreamOut = new MemoryStream();
         decompressionStream.CopyTo(bigStreamOut);
-        byte[] results = bigStreamOut.ToArray();
-        foreach(byte x in results){
-            Debug.Log(x);
+        byte[] results = bigStreamOut.ToArray();                            // Contains #, A, B, and C.
+        int width = results[8];
+        int height = results[12];
+
+        byte[] layer = results.Skip(16).Take(results.Length).ToArray();     // Contains C.
+        string ASCII = "";
+        string Foreground_RGB = "";
+        string Background_RGB = "";
+        for (int i = 0; i < 10; i++){
+            
+            if (i < 4){
+                ASCII = ASCII + layer[i] + " ";
+            } else if (i < 7){
+                Foreground_RGB = Foreground_RGB + layer[i] + " ";
+            } else{
+                Background_RGB = Background_RGB + layer[i] + " ";
+            }
         }
+        Debug.Log(ASCII);
+        Debug.Log(Foreground_RGB);
+        Debug.Log(Background_RGB);
     }
 
     // Update is called once per frame
